d298360bd3bdcb02739c71175ceb15632ff9109d
Artem_orloff <artem_orloff@mail.ru>
2023-06-28 12:15:03 +0300

    fix user, UserRepository and UserServiceImpl.

--- src/main/java/com/example/officelibrary/repository/UserRepository.java
+++ src/main/java/com/example/officelibrary/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.officelibrary.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Integer>{
-    static Object getOne(int id);
+    static Object getOne(int id) {
+        return null;
+    }
 
     boolean existsById(int id);
 
